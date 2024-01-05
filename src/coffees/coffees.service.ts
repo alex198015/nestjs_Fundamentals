be5634c38ab516ac04f2b13d3f841b09cfb7743e
alex198015 <kitten20811@gmail.com>
2024-01-05 23:26:44 +0200

    typeorm entities

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,23 +1,34 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffees.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Flavor } from './entities/flavor.entity';
 
 @Injectable()
 export class CoffeesService {
-    private coffees: Coffee[] = [
-        {
-            id: 1,
-            name: 'Shipwreck Roast',
-            brand: 'Buddy Brew',
-            flavors: ['chocolate', 'vanilla']
-        }
-    ]
+    constructor(
+        @InjectRepository(Coffee)
+        private readonly coffeeRepository: Repository<Coffee>,
 
-    findAll() {
-        return this.coffees
+        @InjectRepository(Flavor)
+        private readonly flavorRepository: Repository<Flavor>
+    ) {}
+
+    async findAll() {
+        return await this.coffeeRepository.find({
+            relations: ['flavors']
+        })
     }
 
-    findOne(id: string) {
-        const coffee = this.coffees.find(item => item.id === +id)
+    async findOne(id: string) {
+        const coffee = await this.coffeeRepository.findOne({
+            where:{id: +id},
+            relations: {
+                flavors: true
+            }
+        })
         if (!coffee) {
             // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND)
             throw new NotFoundException(`Coffee #${id} not found`)
@@ -26,20 +37,47 @@ export class CoffeesService {
         return coffee
     }
 
-    create(createCoffeeDto: any) {
-       this.coffees.push(createCoffeeDto)
-       return createCoffeeDto
+    async create(createCoffeeDto: CreateCoffeeDto) {
+        const flavors = await Promise.all(
+            createCoffeeDto.flavors.map(name => this.preloadFlavorByName(name))
+        )
+
+       const coffee = await this.coffeeRepository.create({
+        ...createCoffeeDto,
+        flavors
+       })
+       return await this.coffeeRepository.save(coffee)
     }
 
-    update(id: string, updateCoffeeDto: any) {
-        const existingCoffee = this.findOne(id)
-        if (existingCoffee) {
-                //
+    async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+
+        const flavors = updateCoffeeDto.flavors && (await Promise.all(
+            updateCoffeeDto.flavors.map(name => this.preloadFlavorByName(name))
+        ))
+
+        const coffee = await this.coffeeRepository.preload({
+            id: +id,
+            ...updateCoffeeDto,
+            flavors
+        })
+
+        if (!coffee) {
+                throw new NotFoundException(`Coffe #${id} not found`)
         }
+        return this.coffeeRepository.save(coffee)
     }
 
-    remove(id: string){ 
-        const coffeeIndex = this.coffees.findIndex(item => item.id === +id)
-        this.coffees.splice(coffeeIndex, 1)
+    async remove(id: string){ 
+        const coffee = await this.findOne(id)
+        return this.coffeeRepository.remove(coffee)
+    }
+
+    private async preloadFlavorByName(name: string): Promise<Flavor> {
+        const existingFlavor = await this.flavorRepository.findOne({where: {name}})
+        if(existingFlavor) {
+            return existingFlavor
+        }
+
+        return this.flavorRepository.create({name})
     }
 }
--- src/coffees/entities/coffees.entity.ts
+++ src/coffees/entities/coffees.entity.ts
@@ -1,6 +1,19 @@
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Flavor } from "./flavor.entity";
+
+@Entity()
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
-    flavors: string[];
+
+    // @Column('json', { nullable: true })
+    @JoinTable()
+    @ManyToMany(type => Flavor, flavor => flavor.coffees, { cascade: true})
+    flavors: Flavor[];
 }
